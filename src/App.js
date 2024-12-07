0412c25afb30429e9a8b7c34d59ad9e383eca4f2
RalphsCode <RalphsCode@gmail.com>
2024-12-06 16:39:01 -0800

    Created and connected EightBall Component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import EightBall from './EightBall';
 
 function App() {
   return (
     <div className="App">
       <h1>RalphsCode Magic 8-Ball</h1>
+      <p><EightBall /></p>
     </div>
   );
-}
+};  // END App()
 
 export default App;
---
+++ src/EightBall.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EightBall = () => {
+    return (
+    <div>
+        <h3>EightBall App is running and connected with the UI</h3>
+    </div>
+) };  // END EightBall()
+
+export default EightBall;
